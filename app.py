a5feee383243fa0b2b8859fb7942d089a5e749a6
Marcin Kucharski <dixone23@gmail.com>
2019-07-17 23:16:57 +0200

    Working functionality

--- app.py
+++ app.py
@@ -1,32 +1,35 @@
+#https://webdesign.tutsplus.com/tutorials/building-a-weather-app-with-the-darksky-api--cms-28678 PRZEKSZTALCENIE LONG,LAT na nazwe miasta z google api
 from flask import Flask, render_template
-import json, time, urllib.request
+import json, time, requests, datetime
 
-API = '1bzYRqzTdOmJyENfmlZY7cm24tGhf5fq'
-legnica = 273127
-
-def getCity(city_name):
-    search_url = 'http://dataservice.accuweather.com/locations/v1/cities/search?apikey=1bzYRqzTdOmJyENfmlZY7cm24tGhf5fq&q=' + city_name + '&details=true'
-    with urllib.request.urlopen(search_url) as search_url:
-        data = json.loads(search_url.read().decode())
-        city_location_key = data[0]['Key']
-    return city_location_key
-
-def getForecast(city_location_key):
-    forecast_url = 'http://dataservice.accuweather.com/forecasts/v1/daily/5day/' + city_location_key + '?apikey=1bzYRqzTdOmJyENfmlZY7cm24tGhf5fq&metric=true'
-    with urllib.request.urlopen(forecast_url) as forecast_city:
-        data = json.loads(forecast_city.read().decode())
-        with open('data.json', 'w') as f:
-            json.dump(data, f)
-        print(data)
+darkskyAPI = 'dacdc5620ba241a45fb5c1f5d838fdf9'
 
 app = Flask(__name__)
 
-#@app.route("/")
+@app.route("/weather")
 def main():
-    #today
-    #nextday1
-    #nextday2
-    #nextday3
-    return render_template("layout.html")#, today=today, nextday1=nextday1, nextday2=nextday2, nextday3=nextday3)
+    weatherUrl = requests.get('https://api.darksky.net/forecast/' + darkskyAPI + '/51.21006,16.1619?lang=pl&units=auto&exclude=currently&exclude=minutely&exclude=hourly&exclude=alerts&exclude=flags')
+    weatherRequest = weatherUrl.text
+    data = json.loads(weatherRequest)
 
-getForecast(getCity('Legnica'))
+    dt1 = data['daily']['data'][0]['time']
+    date1 = time.ctime(dt1)
+    date1 = time.strftime("%D")
+
+    dt2 = data['daily']['data'][1]['time']
+    date2 = time.ctime(dt2)
+    date2 = time.strftime("%D")
+
+    dt3 = data['daily']['data'][2]['time']
+    date3 = time.ctime(dt3)
+    date3 = time.strftime("%D")
+
+    dt4 = data['daily']['data'][3]['time']
+    date4 = time.ctime(dt4)
+    date4 = time.strftime("%D")
+
+    today = data['daily']['data'][0]['temperatureMax']
+    nextday1 = data['daily']['data'][1]['temperatureMax']
+    nextday2 = data['daily']['data'][2]['temperatureMax']
+    nextday3 = data['daily']['data'][3]['temperatureMax']
+    return render_template("layout.html", today=today, nextday1=nextday1, nextday2=nextday2, nextday3=nextday3, date1=date1, date2=date2, date3=date3, date4=date4)
